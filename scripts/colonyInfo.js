4a17c409f201174fb07d95a702e2185e686e21d1
Grace Boaz <anna.g.boaz@gmail.com>
2022-11-07 16:00:47 -0600

    fixed the database typo and added more to colonyInfo

--- scripts/colonyInfo.js
+++ scripts/colonyInfo.js
@@ -1,7 +1,8 @@
 //IMPORTS
-import { getGoverners } from "./database,js";
+import { getGoverners, getColonies, getPurchasedMinerals } from "./database.js";
 
 let activeGoverners = []
+let purchasedMinerals = []
 
 //GOVERNER DROP
 
@@ -38,10 +39,20 @@ mainContainer.addEventListener( "change", (event) => {
         const [,colonyId] = event.target.value.split("--")
         for (const colony of getColonies()){
             if(colony.id === parseInt(colonyId)){
+                findMinerals(colony.id)
                 return `${colony.name}`
             }
         }
     }
 })
+
+// find the minerals that this colony has purchased
+const findMinerals = (colonyId) => {
+    for( const mineral of getPurchasedMinerals()){
+        if (mineral.colonyId === colonyId){
+            
+        }
+    }
+}
 
 
